1a2969913d80bd5f925cd296b14a2b031d667c90
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2024-02-13 17:45:23 +0100

    ajout du spinner

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit} from '@angular/core';
-import { Pokemon } from '../pokemon';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Pokemon } from '../pokemon';
 import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-list-pokemon',
-  templateUrl: './list-pokemon.component.html'
+  templateUrl: './list-pokemon.component.html',
 })
-export class ListPokemonComponent implements OnInit{
+export class ListPokemonComponent implements OnInit {
   pokemonList!: Pokemon[];
 
-  constructor(private router:Router,
-              private pokemonService:PokemonService
-  ){}
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonService
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.pokemonService.getPokemonList()
-      .subscribe(pokemonList=>this.pokemonList = pokemonList);
+      .subscribe(pokemonList => this.pokemonList = pokemonList);
   }
 
-  goToPokemon(pokemon:Pokemon){
-    this.router.navigate(["pokemon",pokemon.id]);
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon', pokemon.id]);
   }
 }
